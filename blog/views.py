34ec0ec74fafaa87b5d4d6a8cd43e69208d611dd
Danil <danchic1556@gmail.com>
2022-12-12 21:51:09 +0300

    Access limitation urls

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,9 @@ from .utils import *
 from .models import Post,Tag
 from .forms import TagForm, PostForm
 
-#Выодит список постов
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 def post_list(requests):
     posts = Post.objects.all()
     paginator = Paginator(posts, 1)
@@ -36,16 +38,18 @@ def post_list(requests):
 
     return render(requests, 'blog/post_list.html', context=context)
 
-class PostUpdate(ObjectsUpdateMixin, View):
+class PostUpdate(LoginRequiredMixin, ObjectsUpdateMixin, View):
     model = Post
     form_model = PostForm
     template = 'blog/post_update.html'
+    raise_exception = True
 
 
 
-class PostCreate(ObjectsCreateMixin, View):
+class PostCreate(LoginRequiredMixin, ObjectsCreateMixin, View):
     form_model = PostForm
     template = 'blog/post_create.html'
+    raise_exception = True
 
 
 class PostDetail(ObjectDetailMixin, View):
@@ -53,16 +57,18 @@ class PostDetail(ObjectDetailMixin, View):
     template = 'blog/post_detail.html'
 
 
-class PostDelete(ObjectDeleteMixin,View):
+class PostDelete(LoginRequiredMixin,ObjectDeleteMixin,View):
     model = Post
     template = 'blog/post_delete.html'
     url = 'posts_list_url'
+    raise_exception = True
 
 
-class TagUpdate(ObjectsUpdateMixin, View):
+class TagUpdate(LoginRequiredMixin, ObjectsUpdateMixin, View):
     model = Tag
     form_model = TagForm
     template = 'blog/tag_update.html'
+    raise_exception = True
 
 
 class TagDetail(ObjectDetailMixin,View):
@@ -70,12 +76,13 @@ class TagDetail(ObjectDetailMixin,View):
     template = 'blog/tag_detail.html'
 
 
-class TagCreate(ObjectsCreateMixin, View):
+class TagCreate(LoginRequiredMixin, ObjectsCreateMixin, View):
     form_model = TagForm
     template = 'blog/tag_create.html'
+    raise_exception = True
 
 
-class TagDelete(ObjectDeleteMixin, View):
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Tag
     template = 'blog/tag_delete.html'
     url = 'tag_list_url'
